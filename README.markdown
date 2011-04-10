0c7c4170dac698f277e83951ba6a08415837e558
Thomas Brewer <tom@th3mus1cman.me>
2011-04-09 23:07:08 -0400

    initial commit

---
+++ README.markdown
@@ -0,0 +1,53 @@
+# Sherlock (BETA) #
+
+Lets you inspect the url.
+
+## Installation
+
+* Copy the /system/expressionengine/third_party/sherlock/ folder to your /system/expressionengine/third_party/ folder
+
+## Usage
+
+* Use to detect the type of page based on the url  
+
+		{exp:sherlock:page_type}
+			{if is_pagination} 
+				There are a lot of these.
+			{/if} 
+			{if is_pagination && is_category}
+				There are a lot of these to filter.
+			{/if} 
+			{if page_type == 'category'} 
+				Lets filter these entries a little.
+			{/if}
+		{/exp:sherlock:page_type}
+
+* Get a category_id based upon a url segment  
+
+		{exp:sherlock:category_id category_url_title="{segment_4}"}
+			{exp:weblog:entries weblog="news" category="{category_id}"}
+				<ul>
+					<li><a href="{url_title_path="news"}">{title}</a></li>
+				</ul>
+			{/exp:weblog:entries}
+		{/exp:sherlock:category_id}
+
+* Get query string variables out of the url  
+
+		{exp:sherlock:get}
+			<p>{get:name} - {get:gender}</p>
+		{/exp:sherlock:get}
+
+
+* You also have access to all the parts of the url  
+
+		{exp:sherlock:parts}
+			<p>{protocal}://{host}:{port}{uri}</p>
+			{if uri_part_1 == 'home'}
+				<p>Home at last!</p>
+			{/if}
+		{/exp:sherlock:parts}
+
+* And you can decode url encode strings  
+
+		{exp:sherlock:url_decode string="Cheese%20is%20awesome!"}
---
+++ pi.sherlock.php
@@ -0,0 +1,293 @@
+<?php
+
+$plugin_info = array(
+  'pi_name' 			=> 'Sherlock',
+  'pi_version' 		=> '1.0',
+  'pi_author' 			=> 'Thomas Brewer',
+  'pi_author_url' 	=> 'http://th3mu1cman.me',
+  'pi_description' 	=> 'Inspects URLs',
+  'pi_usage' 			=> Sherlock::usage()
+);
+
+/**
+ * Sherlock - A EE2 plugin inspects urls
+ *
+ * @package Sherlock
+ * @author Thomas Brewer
+ **/
+class Sherlock {
+	
+	public $return_data = '';
+	
+	protected $page_types = array(
+		'pagination' 		=> FALSE,
+		'category' 			=> FALSE,
+		'archive' 			=> FALSE,
+	);
+	
+	protected $current_type = '';
+	
+	/**
+	 * __construct
+	 *
+	 * @access public
+	 * @param  void	
+	 * @return void
+	 * 
+	 **/
+	public function Sherlock() 
+	{
+		$this->EE =& get_instance();
+	}
+	
+	/**
+	 * page_type
+	 *
+	 * @access public
+	 * @param  void	
+	 * @return void
+	 * 
+	 **/
+	public function page_type() 
+	{		
+		$this->tagdata = $this->EE->TMPL->tagdata;
+
+		$this->category_trigger = $this->EE->config->item("reserved_category_word");
+		
+		$this->segments = $this->EE->uri->segments;
+		
+		foreach ($this->segments as $segment) 
+		{
+			$this->_test_segment($segment);
+		}
+		
+		if (count($this->segments) > 0) 
+		{
+			$category_query = $this->EE->db->select('cat_id')
+				->from('categories')
+				->where_in('cat_url_title', $this->segments)
+				->limit(1)
+				->get();
+				
+				if ($category_query->num_rows() == 1)
+				{
+					$this->page_types['category'] = TRUE;
+					$this->current_type = 'category';
+				}
+		}
+		
+		$conditionals = array();
+		
+		foreach ($this->page_types as $page_type => $cond) 
+		{
+			$conditionals['is_'.$page_type] = $cond;
+		}	
+			
+		$conditionals['page_type'] = $this->current_type;
+	
+		$this->tagdata = $this->EE->functions->prep_conditionals($this->tagdata, $conditionals);
+
+		$this->return_data = str_replace('{page_type}', $this->current_type, $this->tagdata);
+
+		return $this->return_data;
+	}
+	
+	/**
+	 * _test_segment
+	 *
+	 * @access public
+	 * @param  void	
+	 * @return void
+	 * 
+	 **/
+	public function _test_segment($segment) 
+	{
+		if (preg_match('/^[P][0-9]+$/i', $segment))
+		{
+			$this->page_types['pagination'] = TRUE;
+			$this->current_type = 'pagination';
+			return TRUE;
+		}
+		else if (preg_match("/$this->category_trigger/", $segment))
+		{
+			$this->page_types['category'] = TRUE;
+			$this->current_type = 'category';
+			return TRUE;
+		}
+		else if (preg_match("/^\d{4}$/", $segment))
+		{
+			$this->page_types['archive'] = TRUE;
+			$this->current_type = 'archive';
+			return TRUE;
+		}
+		
+		return FALSE;
+	}
+	
+	/**
+	 * category_id
+	 *
+	 * @access public
+	 * @param  void	
+	 * @return void
+	 * 
+	 **/
+	public function category_id() 
+	{
+		$category_url_title = $this->EE->TMPL->fetch_param('category_url_title', NULL);
+		
+		$cat_id = '';
+		
+		if ($category_url_title !== NULL && !empty($category_url_title))
+		{
+			$category_query = $this->EE->db->select('cat_id')
+				->from('categories')
+				->where('cat_url_title', $category_url_title)
+				->limit(1)
+				->get();
+				
+				if ($category_query->num_rows() == 1)
+				{
+					$cat = $category_query->row_array();
+					$cat_id = $cat['cat_id'];
+				}
+		}
+		
+		$tagdata = $this->EE->TMPL->tagdata;
+		
+		if (empty($tagdata))
+		{
+			return $cat_id;
+		}
+		else
+		{
+			return $this->EE->TMPL->parse_variables_row($tagdata, array('category_id' => $cat_id));
+		}
+	}
+	
+	/**
+	 * get
+	 *
+	 * @access public
+	 * @param  void	
+	 * @return void
+	 * 
+	 **/
+	public function get() 
+	{
+		$data = array();
+		
+		foreach (array_keys($_GET) as $key) 
+		{
+			$data['get:'.$key] = $this->EE->input->get($key, TRUE);
+		}
+		
+		$this->return_data = $this->EE->TMPL->parse_variables_row($this->EE->TMPL->tagdata, $data);
+
+		//remove all the {get:****} vars
+		$this->return_data = preg_replace('/{get:.*}/Usi', '', $this->return_data);
+		
+		return $this->return_data;
+	}
+	
+	/**
+	 * parts
+	 *
+	 * @access public
+	 * @param  void	
+	 * @return void
+	 * 
+	 **/
+	public function parts() 
+	{					
+		$uri = (isset($_SERVER["PATH_INFO"])) ? $_SERVER["PATH_INFO"] : '';
+		
+		$protocal = (isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on") ? 'https' : 'http';
+		
+		$data = array(
+			'host' 		=> (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : '',
+			'port' 		=> (isset($_SERVER['SERVER_PORT'])) ? $_SERVER['SERVER_PORT'] : '',
+			'uri'			=> $uri,
+			'protocal' 	=> $protocal,
+		);
+	
+		foreach (array_filter(explode('/', $uri)) as $count => $url_part) 
+		{
+			$data['uri_part_'.$count] = $url_part;
+		}
+		
+		return $this->EE->TMPL->parse_variables_row($this->EE->TMPL->tagdata, $data);
+	}
+	
+	/**
+	 * url_decode
+	 *
+	 * @access public
+	 * @param  void	
+	 * @return void
+	 * 
+	 **/
+	public function url_decode() 
+	{
+		return urldecode($this->EE->TMPL->fetch_param('string', ''));
+	}
+	
+	
+	/**
+	 * usage
+	 *
+	 * @access public
+	 * @param  void	
+	 * @return string
+	 * 
+	 **/
+	public static function usage() 
+	{
+		return <<< HTML
+		{exp:sherlock:page_type}
+
+			{if is_pagination}
+				<p>There are a lot of these.</p>
+			{/if}
+
+			{if page_type == 'category'}
+				<p>Lets filter these entries a little.</p>
+			{/if}
+
+		{/exp:sherlock:page_type}
+
+		{exp:sherlock:category_id category_url_title="{segment_4}"}
+
+			{exp:weblog:entries weblog="news" category="{category_id}"}
+				<ul>
+					<li><a href="{url_title_path="news"}">{title}</a></li>
+				</ul>
+			{/exp:weblog:entries}
+
+		{/exp:sherlock:category_id}
+
+
+		{exp:sherlock:get}
+
+			<p>{get:name} - {get:gender}</p>
+
+		{/exp:sherlock:get}	
+
+
+		{exp:sherlock:parts}
+
+			<p>{protocal}://{host}:{port}{uri}</p>
+
+			{if uri_part_1 == 'home'}
+				<p>Home at last!</p>
+			{/if}
+
+		{/exp:sherlock:parts}
+
+
+		{exp:sherlock:url_decode string="Cheese%20is%20awesome!"}	
+HTML;
+		
+	}
+	
+}
